4933e93d3e79ee32f5c3d54d8740ca10f5943300
Marcin Skurski <marcin.skurski@smartbox.com>
2015-10-13 10:29:21 +0000

    adding array format to jms serializer (serializing object to array and deserializing array to object)

---
+++ Serializer/ArrayDeserializationVisitor.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Smartbox\CoreBundle\Serializer;
+
+use JMS\Serializer\Construction\ObjectConstructorInterface;
+use JMS\Serializer\Context;
+use JMS\Serializer\GenericDeserializationVisitor;
+use JMS\Serializer\Naming\PropertyNamingStrategyInterface;
+
+/**
+ * Class ArrayDeserializationVisitor
+ * @package Smartbox\CoreBundle\Serializer
+ */
+class ArrayDeserializationVisitor extends GenericDeserializationVisitor
+{
+    /**
+     * @var DeserializationTypesValidator
+     */
+    protected $visitorValidator;
+
+    /**
+     * @param PropertyNamingStrategyInterface $namingStrategy
+     * @param ObjectConstructorInterface $objectConstructor
+     * @param DeserializationTypesValidator $visitorValidator
+     */
+    public function __construct(
+        PropertyNamingStrategyInterface $namingStrategy,
+        ObjectConstructorInterface $objectConstructor,
+        DeserializationTypesValidator $visitorValidator
+    ) {
+        parent::__construct($namingStrategy);
+
+        $visitorValidator->setNamingStrategy($this->namingStrategy);
+        $this->visitorValidator = $visitorValidator;
+    }
+
+    protected function decode($value)
+    {
+        return (array) $value;
+    }
+
+    public function visitString($data, array $type, Context $context)
+    {
+        $this->visitorValidator->validateString($data, $context, $this->getCurrentObject());
+
+        return parent::visitString($data, $type, $context);
+    }
+
+    public function visitBoolean($data, array $type, Context $context)
+    {
+        $this->visitorValidator->validateBoolean($data, $context, $this->getCurrentObject());
+
+        return parent::visitBoolean($data, $type, $context);
+    }
+
+    public function visitDouble($data, array $type, Context $context)
+    {
+        $this->visitorValidator->validateDouble($data, $context, $this->getCurrentObject());
+
+        return parent::visitDouble($data, $type, $context);
+    }
+
+    public function visitInteger($data, array $type, Context $context)
+    {
+        $this->visitorValidator->validateInteger($data, $context, $this->getCurrentObject());
+
+        return parent::visitInteger($data, $type, $context);
+    }
+}
---
+++ Tests/Serializer/ArraySerializationVisitorFunctionalTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Smartbox\CoreBundle\Tests\Serializer;
+
+use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerBuilder;
+use JMS\Serializer\SerializerInterface;
+use Smartbox\CoreBundle\Serializer\ArraySerializationVisitor;
+use Smartbox\CoreBundle\Tests\Fixtures\Entity\EntityConstants;
+use Smartbox\CoreBundle\Tests\Fixtures\Entity\TestEntity;
+
+class ArraySerializationVisitorFunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var SerializerInterface $serializer */
+    private $serializer;
+
+    public function setup()
+    {
+        $builder = new SerializerBuilder();
+
+        /** @var \JMS\Serializer\Construction\ObjectConstructorInterface|\PHPUnit_Framework_MockObject_MockObject $objectConstructor */
+        $objectConstructor = $this->getMockBuilder('\JMS\Serializer\Construction\ObjectConstructorInterface')->getMock();
+
+        $this->serializer = $builder
+            ->setSerializationVisitor(
+                'array',
+                new ArraySerializationVisitor(
+                    new IdenticalPropertyNamingStrategy(),
+                    $objectConstructor
+                )
+            )
+            ->addMetadataDir(__DIR__.'/../Fixtures/Entity', 'Smartbox\CoreBundle\Tests\Fixtures\Entity')
+            ->build()
+        ;
+    }
+
+
+    public function testItShouldSerializeValidEntity()
+    {
+        $data = new TestEntity();
+        $data->setTitle('some title');
+        $data->setDescription('some description');
+        $data->setNote('some note');
+
+        $arrayData = $this->serializer->serialize($data, 'array');
+        $this->assertEquals(
+            [
+                'type' => TestEntity::class,
+                'title' => 'some title',
+                'description' => 'some description',
+                'note' => 'some note',
+            ],
+            $arrayData
+        );
+    }
+
+    public function testItShouldSerializeValidEntityWithVersion()
+    {
+        $data = new TestEntity();
+        $data->setTitle('some title');
+        $data->setDescription(22);
+        $data->setNote('some note');
+
+        // description is not valid but it's introduced in V2 of the entity
+        // we will deserialize for V1 so the error should not be triggered
+
+        $context = new SerializationContext();
+        $context->setVersion(EntityConstants::VERSION_1);
+
+        $arrayData = $this->serializer->serialize($data, 'array', $context);
+        $this->assertEquals(
+            [
+                'type' => TestEntity::class,
+                'title' => 'some title',
+                'note' => 'some note',
+            ],
+            $arrayData
+        );
+    }
+
+    public function testItShouldSerializeValidEntityWithGroup()
+    {
+        $data = new TestEntity();
+        $data->setTitle(11);
+        $data->setDescription('some description');
+        $data->setNote(33);
+
+        // Title and note are not valid valid but they are not available in the group B so the error
+        // should not be triggered
+
+        $context = new SerializationContext();
+        $context->setGroups([EntityConstants::GROUP_C]);
+
+        $arrayData = $this->serializer->serialize($data, 'array', $context);
+        $this->assertEquals(
+            [
+                'description' => 'some description',
+            ],
+            $arrayData
+        );
+    }
+}
