1a89f3ed30a15350715994dc1e5842471655078c
Jose <jose.rufino@smartbox.com>
2016-03-10 17:51:16 +0000

    Review changes

--- DependencyInjection/SmokeTestCompilerPass.php
+++ DependencyInjection/SmokeTestCompilerPass.php
@@ -2,7 +2,6 @@
 
 namespace Smartbox\CoreBundle\DependencyInjection;
 
-use Smartbox\CoreBundle\Utils\SmokeTest\Generic\ConnectivityCheckSmokeTest;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -23,14 +22,17 @@ class SmokeTestCompilerPass implements CompilerPassInterface
 
         $serviceIds = $container->findTaggedServiceIds('smartbox.smoke_test');
         foreach ($serviceIds as $serviceId => $tags) {
-            $smokeTestCommand->addMethodCall('addTest', [$serviceId, new Reference($serviceId)]);
-        }
+            foreach($tags as $tag => $attr){
+                $runMethod = 'run';
+                $descriptionMethod = 'getDescription';
 
-        // initialize smartcore.smoke_test.generic.connectivity_check with items which needs to check connectivity
-        $connectivityCheckSmokeTestDef = $container->getDefinition('smartcore.smoke_test.generic.connectivity_check');
-        $connectivityCheckSmokeTestItems = $container->findTaggedServiceIds(ConnectivityCheckSmokeTest::TAG_ITEM);
-        foreach ($connectivityCheckSmokeTestItems as $serviceName => $tags) {
-            $connectivityCheckSmokeTestDef->addMethodCall('addItem', array($serviceName, new Reference($serviceName)));
+                if(array_key_exists('method',$attr)){
+                    $runMethod = $attr['runMethod'];
+                    $descriptionMethod = $attr['descriptionMethod'];
+                }
+
+                $smokeTestCommand->addMethodCall('addTest', [$serviceId, new Reference($serviceId),$runMethod, $descriptionMethod]);
+            }
         }
     }
 }
