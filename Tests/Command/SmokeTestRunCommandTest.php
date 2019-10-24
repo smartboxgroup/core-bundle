c87a95f70766e03e54b95ccbf83cb128cdf292be
David Camprubi <david.camprubi@smartbox.com>
2019-10-24 23:06:17 +0100

    Clean up smoke test

--- Tests/Command/SmokeTestRunCommandTest.php
+++ Tests/Command/SmokeTestRunCommandTest.php
@@ -9,7 +9,6 @@ use Smartbox\CoreBundle\Utils\SmokeTest\SmokeTestInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Smartbox\CoreBundle\Command\SmokeTestRunCommand;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -19,44 +18,26 @@ use Symfony\Component\Console\Tester\CommandTester;
  */
 class SmokeTestRunCommandTest extends KernelTestCase
 {
-    /**
-     * @var Application
-     */
-    private $application;
-
-    /**
-     * @var array
-     */
-    private $commandConfiguration = [];
-
-    /**
-     * @var Command
-     */
-    private $command;
-
-    /**
-     * @var CommandTester
-     */
-    private $commandTester;
-
-    public function setUp()
+    public function testExecuteCommand()
     {
         $kernel = $this->createKernel();
         $kernel->boot();
 
-        $this->application = new Application($kernel);
-        $this->application->add(new SmokeTestRunCommand());
+        $application = new Application($kernel);
+        $application->add(new SmokeTestRunCommand());
 
-        $this->command = $this->application->find('smartbox:smoke-test');
-        $this->commandTester = new CommandTester($this->command);
-    }
+        $command = $application->find('smartbox:smoke-test');
 
-    public function tearDown()
-    {
-        $this->application = null;
-        $this->command = null;
-        $this->commandTester = null;
-        $this->commandConfiguration = null;
+        $input = ['command' => $command->getName()];
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute($input);
+
+        $output = $commandTester->getDisplay();
+
+        $this->assertInternalType('string', $output);
+        $this->assertContains('Smoke Tests', $output);
+        $this->assertNotContains('Error', $output);
     }
 
     public function testSmokeTestCommandNoLabels()
@@ -120,42 +101,5 @@ class SmokeTestRunCommandTest extends KernelTestCase
         $smokeTestRunCommand->addTest('id2', $smokeTest2, 'run', 'getDescription', ['critical']);
 
         $smokeTestRunCommand->run(new ArrayInput(['--all' => true]), new NullOutput());
-    }
-
-    public function testExecute()
-    {
-        $output = $this->execute();
-
-        $this->assertInternalType('string', $output);
-        $this->assertContains('Smoke Tests', $output);
-        $this->assertNotContains('Error', $output);
-    }
-
-    public function testExecuteWithLabels()
-    {
-        $label = ['-l' => 'important'];
-
-        $this->setCommandConfiguration($label);
-        $output = $this->execute();
-
-        $this->assertInternalType('string', $output);
-        $this->assertContains('Smoke Tests', $output);
-        $this->assertNotContains('Error', $output);
-    }
-
-    private function execute()
-    {
-        $this->commandTester->execute([
-            'command' => $this->command->getName(),
-        ],
-            $this->commandConfiguration
-        );
-
-        return $this->commandTester->getDisplay();
-    }
-
-    private function setCommandConfiguration($options = [])
-    {
-        $this->commandConfiguration = $options;
     }
 }
