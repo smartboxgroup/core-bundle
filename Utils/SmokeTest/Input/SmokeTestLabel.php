cae0a68d468256e69ae403d14c0f8c55fb257a56
David Camprubi <david.camprubi@smartbox.com>
2019-10-24 14:56:20 +0100

    Delete empty label constant

--- Utils/SmokeTest/Input/SmokeTestLabel.php
+++ Utils/SmokeTest/Input/SmokeTestLabel.php
@@ -10,7 +10,6 @@ class SmokeTestLabel
     /**
      * The label defines the critical level for smoke tests.
      */
-    const LABEL_EMPTY = '';
     const LABEL_WIP = 'wip';
     const LABEL_IMPORTANT = 'important';
     const LABEL_CRITICAL = 'critical';
