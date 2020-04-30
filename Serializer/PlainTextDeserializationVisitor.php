1f6f9e2e346b7257adb3451e9f98e6a125577080
Andres Rey <andres.rey@smartbox.com>
2020-04-30 16:49:01 +0100

    MINT-4660: Introduce a PlainTextDeserializationVisitor that allows "deserialization" of plain text responses. (Like Gambit, DSV, etc does)

---
+++ Serializer/PlainTextDeserializationVisitor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Smartbox\CoreBundle\Serializer;
+
+/**
+ * Class JsonDeserializationVisitor.
+ */
+class PlainTextDeserializationVisitor extends \JMS\Serializer\JsonDeserializationVisitor
+{
+    protected function decode($str)
+    {
+        return $str;
+    }
+
+}
